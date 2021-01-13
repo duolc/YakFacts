d508e3f7ed8068e1ea6fa2cd3d60452dbfd2e321
Ryan Johnson <Ryan.Johnson@pridesurveys.com>
2021-01-12 22:03:31 -0500

    Initial Launch
    Version 1.0

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,7 +1,16 @@
-$(document).ready(function() {
-	$.getJSON('assets/js/yakfacts.json', function(jd) {
-		 $('#factoids').html('<p> Name: ' + jd.id + '</p>');
-		 $('#factoids').append('<p>Age : ' + jd.fact + '</p>');
-		 $('#factoids').append('<p> Sex: ' + jd.source + '</p>');
-	   });
-  });
+$.getJSON( "assets/js/yakfacts.json", function( data ) {
+	/* Load the initial Fact */
+	var numOfFacts = data.length - 1
+	var intialRand = Math.floor((Math.random() * numOfFacts) + 0);
+	chosenFact = data[ intialRand ];
+	$( ".thefact" ).replaceWith( `<h2 class="thefact">${chosenFact.fact}</h2>` );
+	$( ".thesource" ).replaceWith( `<h5 class="thesource">Source: <a href="${chosenFact.source}">${chosenFact.source}</a></h5>` );
+	
+	/* Load New Fact On Click */
+	$( "#yakAttack" ).click(function() {
+		var newRand = Math.floor((Math.random() * numOfFacts) + 0);
+		newFact = data[ newRand ];
+		$( ".thefact" ).replaceWith( `<h2 class="thefact">${newFact.fact}</h2>` );
+		$( ".thesource" ).replaceWith( `<h5 class="thesource">Source: <a href="${newFact.source}">${newFact.source}</a></h5>` );
+	});
+});
